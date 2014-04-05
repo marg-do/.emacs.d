2a1c7d5b02164fbee1d74c2ce912df2ffeebadcc
marg_do <t.t.mc1192.sf@gmail.com>
2014-04-05 10:34:52 +0000

    ghc-mod

--- init.el
+++ init.el
@@ -12,6 +12,14 @@
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
+;;; popup
+(add-to-list 'load-path "~/.emacs.d/elisp/popup-el/")
+(require 'popup)
+
+;; auto-complete
+(add-to-list 'load-path "~/.emacs.d/elisp/auto-complete/")
+(require 'auto-complete-config)
+(ac-config-default)
 
 ;; haskell-mode
 (add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
@@ -21,3 +29,11 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;; symbols
 (setq haskell-font-lock-symbols t)
+;;; ghc-mod
+(add-to-list 'exec-path (concat (getenv "HOME") "/.cabal/bin"))
+(add-to-list 'load-path "~/.cabal/share/ghc-mod-4.0.1")
+;;;;(autoload 'ghc-init "ghc" nil t)
+;;;;(ghc-init)
+(require 'ghc)
+(add-to-list 'ac-sources 'ac-source-ghc-mod)
+	     
