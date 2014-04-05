35a0c3187961d2ba8de1a0a745d3fe416ef64c3c
marg_do <t.t.mc1192.sf@gmail.com>
2014-04-05 06:14:37 +0000

    haskell-mode

--- init.el
+++ init.el
@@ -1,3 +1,23 @@
-; disabled backup
+;; disable backup
 (setq make-backup-files nil)
 (setq auto-save-default nil)
+
+;; load-path
+(add-to-list 'load-path "~/.emacs.d/elisp/")
+
+;; package
+(require 'package)
+;;; for haskell-mode
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+
+;; haskell-mode
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
+;;; indent
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;; symbols
+(setq haskell-font-lock-symbols t)
