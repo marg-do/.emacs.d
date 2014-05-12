933d39e945b4e7ba062abfb91aceeb4e653aceb0
marg_do <t.t.mc1192.sf@gmail.com>
2014-05-12 16:57:09 +0900

    make init.el

---
+++ init.el
@@ -0,0 +1,17 @@
+;; coding: utf-8
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Emacs Base Configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Set Language
+(set-language-environment 'Japanese)
+(prefer-coding-system 'utf-8)
+
+;; Avoid making backups
+(setq make-backup-files nil)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Elisp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
